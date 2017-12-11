7ca5a5c3b314b070a97e7e1393a8153409af7308
trungnghia112 <trungnghia112@gmail.com>
2017-12-11 15:56:40 +0700

    upgrade: angular 5.1 & @angular/service-worker

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,12 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    try {
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/ngsw-worker.js') ;
+      }
+    } catch (e) { }
+
+  })
   .catch(err => console.log(err));

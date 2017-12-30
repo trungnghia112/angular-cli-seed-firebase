7e41ab04fd6914a7cfdf1d7435eebcbe5f61516c
trungnghia112 <trungnghia112@gmail.com>
2017-12-30 14:25:55 +0700

    add: informing the user a new version is available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { environment } from '@env/environment';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,17 @@ import { environment } from '@env/environment';
 export class AppComponent {
   title = 'app';
 
-  constructor() {
+  constructor(private swUpdate: SwUpdate) {
     console.log(environment);
+
+    // check back to the server to see if a new ngsw.json is available
+    // this can be called inside setInterval, upon router navigation, etc.
+    // this.swUpdate.checkForUpdate();
+
+    this.swUpdate.available.subscribe(() => {
+      if (confirm('New version available. Load New Version?')) {
+        window.location.reload();
+      }
+    });
   }
 }

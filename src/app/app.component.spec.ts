3ddb6dfe68c83edf045d19afcdbe8794e6a69d3c
trungnghia112 <trungnghia112@gmail.com>
2019-01-21 12:57:00 +0700

    update run test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ServiceWorkerModule.register('', { enabled: false })
       ],
       declarations: [
         AppComponent

fafb1e6c616da01872e780b16c76771a7c90406b
trungnghia112 <trungnghia112@gmail.com>
2019-03-22 20:27:12 +0700

    add routeAnimations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'about', component: AboutPageComponent },
+  // { path: 'right', component: RightPageComponent, data: { animation: 'isRight' } },
+  // { path: 'left', component: LeftPageComponent, data: { animation: 'isLeft' } },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import { RouterOutlet } from '@angular/router';
+
+import { stepper } from './core/animations/route-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    // fader,
+    // slider,
+    // transformer,
+    stepper
+  ]
 })
 export class AppComponent {
   title = 'angular-cli-seed';
@@ -15,5 +24,9 @@ export class AppComponent {
         updates.activateUpdate().then(() => document.location.reload());
       }
     });
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
   }
 }
